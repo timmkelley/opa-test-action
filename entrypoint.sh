f0915cb4205315a5b25807bc9019475845c498cd
Tim Kelley <69865739+timmkelley@users.noreply.github.com>
2020-11-09 22:22:42 -0500

    Use input args and remove extra

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,21 +4,5 @@ printf "\n\n"
 echo "# Open Policy Agent"
 /opa version
 printf "\n\n"
-echo "Dir:"
-ls -l
-printf "\n\n"
 
-IFS=';'
-mapfile -t lines < <(echo "$INPUT_TESTS" | grep -v "^$")
-
-e_code=0
-for line in "${lines[@]}"; do
-  read -r -a args <<< "$line"
-  cmd="/opa $INPUT_CMD ${args[@]} $INPUT_OPTIONS"
-  echo " 🚀 Running: $cmd"
-  printf "\n"
-  eval "$cmd" || e_code=1
-  printf "\n\n"
-done
-
-exit $e_code
+opa "$1" "$2" "$3"
