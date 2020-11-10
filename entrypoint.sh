e885286b5b7c6a3f92648d431f0989afe8980ed8
Tim Kelley <69865739+timmkelley@users.noreply.github.com>
2020-11-09 22:05:43 -0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,9 @@ printf "\n\n"
 echo "# Open Policy Agent"
 /opa version
 printf "\n\n"
+echo "Dir:"
+pwd
+printf "\n\n"
 
 IFS=';'
 mapfile -t lines < <(echo "$INPUT_TESTS" | grep -v "^$")
@@ -11,7 +14,7 @@ mapfile -t lines < <(echo "$INPUT_TESTS" | grep -v "^$")
 e_code=0
 for line in "${lines[@]}"; do
   read -r -a args <<< "$line"
-  cmd="/opa test ${args[@]} $INPUT_OPTIONS"
+  cmd="/opa $INPUT_CMD ${args[@]} $INPUT_OPTIONS"
   echo " 🚀 Running: $cmd"
   printf "\n"
   eval "$cmd" || e_code=1
