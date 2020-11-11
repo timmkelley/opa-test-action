da57dcddb4e8624ea0302334fdb380af61a98b53
Tim Kelley <69865739+timmkelley@users.noreply.github.com>
2020-11-11 11:22:49 -0500

    Put back to original

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,19 @@
-#!/usr/bin/env sh
-
 printf "\n\n"
 echo "# Open Policy Agent"
 /opa version
 printf "\n\n"
 
-/opa "$1" "$2" "$3"
+IFS=';'
+mapfile -t lines < <(echo "$INPUT_TESTS" | grep -v "^$")
+
+e_code=0
+for line in "${lines[@]}"; do
+  read -r -a args <<< "$line"
+  cmd="/opa test ${args[@]} $INPUT_OPTIONS"
+  echo " 🚀 Running: $cmd"
+  printf "\n"
+  eval "$cmd" || e_code=1
+  printf "\n\n"
+done
+
+exit $e_code
